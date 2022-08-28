ba16077fe55c5d27e6ba29565af95d5f4e0f6753
倪风 <194908655@qq.com>
2022-08-28 16:07:41 +0800

    嵌套一层div就无法使用了

--- app.vue
+++ app.vue
@@ -1,24 +1,26 @@
 <template>
-  <el-button text @click="dialogVisible = true"
-    >click to open the Dialog</el-button
-  >
+  <div>
+    <el-button text @click="dialogVisible = true"
+      >click to open the Dialog</el-button
+    >
 
-  <el-dialog
-    v-model="dialogVisible"
-    title="Tips"
-    width="30%"
-    :before-close="handleClose"
-  >
-    <span>This is a message</span>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="dialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogVisible = false"
-          >Confirm</el-button
-        >
-      </span>
-    </template>
-  </el-dialog>
+    <el-dialog
+      v-model="dialogVisible"
+      title="Tips"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <span>This is a message</span>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="dialogVisible = false"
+            >Confirm</el-button
+          >
+        </span>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script lang="ts" setup>
